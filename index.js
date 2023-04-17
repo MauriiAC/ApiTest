c83e89efff06545f34e56b1cbbdafba1d2a62bce
MauriiAC <mauriciocuello91@gmail.com>
2023-04-17 19:12:47 -0300

    delete first endpoints

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 // Permitir solicitudes desde múltiples orígenes
 app.use(cors({
-  origin: ['http://localhost:3001', 'https://miapp2.com']
+  origin: '*'
 }));
 
 // Configuración de opciones de cors pre-vuelo (preflight)
@@ -14,17 +14,6 @@ app.options('*', cors());
 
 // Middleware para parsear el body de los requests
 app.use(express.json());
-
-app.get('/hello', (req, res) => {
-  res.json({ message: 'Hola mundo!' });
-});
-
-app.get('/sum/:op1/:op2', (req, res) => {
-
-  const {op1, op2} = req.params
-
-  res.json({ result: `${parseInt(op1) + parseInt(op2)}` });
-});
 
 app.listen(PORT, () => {
   console.log(`Servidor Express iniciado en el puerto ${PORT}`);
