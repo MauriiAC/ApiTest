09664e265c09c1410289f20aa6645b77a88bfd6a
MauriiAC <mauriciocuello91@gmail.com>
2023-04-17 19:54:22 -0300

    change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // Permitir solicitudes desde múltiples orígenes
 app.use(cors({
